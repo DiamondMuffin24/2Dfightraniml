bd322299378208cb2e0e9468c9b9a622ecc349e1
DiamondMuffin24 <146829200+DiamondMuffin24@users.noreply.github.com>
2025-01-21 15:46:03 +0000

    V0
    yay

---
+++ Assets/Scripts/HealthControl.cs
@@ -0,0 +1,214 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HealthControl : MonoBehaviour {
+
+	public Image healthBar;
+	public float health; //between 0-100
+	public GameObject restartDialog;
+	public bool canDamage = true;
+	//public float invTime; //Later for better control I can change this to Private, and edit the invTime depending on moves, allowing some moves
+	//to combo and others to not.
+
+	public KeyCode blockButton;
+	public bool isBlocking;
+
+	Rigidbody2D rigi;
+	float yRotation;
+
+	Animator anim;
+
+	public string playerTag;
+	// Use this for initialization
+	void Start() {
+		ShowRestartDialog(false);
+		rigi = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+
+		if (blockButton == KeyCode.None) {
+			blockButton = KeyCode.B;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		CheckHealth ();
+		yRotation = GameObject.FindGameObjectWithTag(playerTag).gameObject.transform.rotation.y;
+		Blocking ();
+	}
+
+	void Blocking(){
+		if (Input.GetKeyUp (blockButton)) {
+			isBlocking = false;
+			canDamage = true;
+
+		}
+
+		else if (Input.GetKeyDown(blockButton)){
+			isBlocking = true;
+			canDamage = false;
+		}
+
+	}
+
+	void CheckHealth() {
+		healthBar.rectTransform.localScale = new Vector3 (health / 100, healthBar.rectTransform.localScale.y,
+			healthBar.rectTransform.localScale.z);
+		if (health <= 0.0f) {
+			ShowRestartDialog (true);
+		}
+	}
+
+	IEnumerator ApplyDamage(float dmg, float invTime)
+	{
+		if (canDamage == true) {
+
+			SubtractHealth (dmg);
+			canDamage = false;
+			anim.SetBool ("Damaged", true);
+			yield return new WaitForSeconds (invTime);
+			anim.SetBool ("Damaged", false);
+			canDamage = true;
+		} 
+		else if (canDamage == false) {
+			yield return new WaitForSeconds (invTime);
+		}
+
+	}
+
+	public void knockBack(float x, float y)
+	{
+		rigi.velocity = Vector3.zero;
+		if (yRotation == 0)
+		{
+			rigi.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
+		}
+		else
+		{
+			rigi.AddForce(new Vector2(-x, y), ForceMode2D.Impulse);
+		}
+	}
+
+	// How to do damage, change the ApplyDamage to be depending on the move.
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.CompareTag("StandMed"))
+		{
+			if (isBlocking == false) {
+				StartCoroutine (ApplyDamage (12, 0.3f));
+				knockBack (3, 5);
+			} 
+			else if (isBlocking == true){
+
+				StartCoroutine (ApplyDamage (0, 0.1f));
+				knockBack (1, 0);
+			}
+		}
+		if (coll.gameObject.CompareTag("StandHeavy"))
+		{
+			if (isBlocking == false)
+			{
+				StartCoroutine(ApplyDamage(25, 1.2f));
+				knockBack(5, 10);
+			}
+			else if (isBlocking == true){
+
+				StartCoroutine (ApplyDamage (0, 0.1f));
+				knockBack (5, 0);
+			}
+		}
+		if (coll.gameObject.CompareTag("LightKick"))
+		{
+			if (isBlocking == false)
+			{
+				StartCoroutine(ApplyDamage(9, 0.3f));
+				knockBack(2, 2);
+			}
+			else if (isBlocking == true){
+
+				StartCoroutine (ApplyDamage (0, 0.1f));
+				knockBack (2, 0);
+			}
+		}
+		if (coll.gameObject.CompareTag("Fireball"))
+		{
+			if (canDamage)
+			{
+				if (isBlocking == false) 
+				{
+					StartCoroutine (ApplyDamage (8, 0.3f));
+					knockBack (1, 0);
+				}
+				else if (isBlocking == true){
+
+					StartCoroutine (ApplyDamage (0, 0.1f));
+					knockBack (1, 0);
+				}
+			}
+
+		}
+		if (coll.gameObject.CompareTag("JumpHeavy"))
+		{
+			if (canDamage)
+			{
+				if (isBlocking == false)
+				{
+					StartCoroutine (ApplyDamage (10, 0.3f));
+					knockBack (3, 0);
+				}
+				else if (isBlocking == true){
+
+					StartCoroutine (ApplyDamage (0, 0.1f));
+					knockBack (3, 0);
+				}
+			}
+		}
+	}
+
+	public void AddHealth(float amount) {
+		if (health + amount > 100.0f) {
+			health = 100.0f;
+		} 
+		else {
+			health += amount;
+		}
+	}
+
+	public void SubtractHealth(float amount) {
+		if (health - amount < 0.0f) {
+			health = 0.0f;
+		} 
+		else {
+			health -= amount;
+		}
+	}
+
+	public void ShowRestartDialog(bool c) {
+		if (c) {
+			Time.timeScale = 0.0f;
+		} else {
+			Time.timeScale = 1.0f;
+		}
+		restartDialog.SetActive (c);
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene("FighterScene");
+	}
+
+	/*
+    public IEnumerator KnockBack(float knockDur, float knockPwr, Vector2 knockDir)
+    {
+        float timer = 0;
+        while (knockDur > timer)
+        {
+            timer += Time.deltaTime;
+            rigi.AddForce(new Vector2(knockDir.x * -100, knockDir.y * knockPwr));
+        }
+        yield return 0;
+    }
+    */
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,187 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    Animator anim;
+
+    private float attackRate;
+    bool[] attack = new bool[5];
+    float[] attackTimer = new float[5];
+    int[] timesPressed = new int[5];
+
+    public Transform firePoint;
+    public GameObject fireBall;
+
+    public float jumpHeight;
+    public bool isJumping = false;
+    private bool canDash;
+    public bool DoubleJump = false; // Toggleable in the Unity Editor
+    private bool hasDoubleJumped = false; // Tracks if the player has double-jumped
+
+    public KeyCode projectileShoot;
+    public KeyCode attackOne;
+    public KeyCode attackTwo;
+    public KeyCode attackThree;
+    public KeyCode attackFour;
+    public KeyCode leftMove;
+    public KeyCode rightMove;
+    public KeyCode jumpButton;
+    public KeyCode dashButton;
+
+    public Rigidbody2D player;
+    public Rigidbody2D otherPlayer;
+
+    public float dashDistance;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (projectileShoot == KeyCode.None) { projectileShoot = KeyCode.U; }
+        if (attackOne == KeyCode.None) { attackOne = KeyCode.I; }
+        if (attackTwo == KeyCode.None) { attackTwo = KeyCode.O; }
+        if (attackThree == KeyCode.None) { attackThree = KeyCode.P; }
+        if (leftMove == KeyCode.None) { leftMove = KeyCode.A; }
+        if (rightMove == KeyCode.None) { rightMove = KeyCode.D; }
+        if (jumpButton == KeyCode.None) { jumpButton = KeyCode.W; }
+        if (dashButton == KeyCode.None) { dashButton = KeyCode.V; }
+        if (attackFour == KeyCode.None) { attackFour = KeyCode.J; }
+    }
+
+    void Update()
+    {
+        Movement();
+        AttackInput();
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(jumpButton))
+        {
+            if (!isJumping) // First jump
+            {
+                JumpAction();
+                isJumping = true;
+            }
+            else if (DoubleJump && !hasDoubleJumped) // Double jump
+            {
+                JumpAction();
+                hasDoubleJumped = true;
+                anim.SetTrigger("DoubleJump"); // Optional animation trigger for double-jump
+            }
+        }
+    }
+
+    void JumpAction()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        anim.SetBool("OnGround", false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+            hasDoubleJumped = false; // Reset double jump on landing
+            canDash = true;
+            anim.SetBool("OnGround", true);
+        }
+    }
+
+    void Movement()
+    {
+        if (!isJumping)
+        {
+            if (Input.GetKey(rightMove))
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                transform.eulerAngles = new Vector2(0, 0);
+                anim.SetFloat("Movement", 1f);
+            }
+            else if (Input.GetKey(leftMove))
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                transform.eulerAngles = new Vector3(0, 180);
+                anim.SetFloat("Movement", 1f);
+            }
+            else
+            {
+                anim.SetFloat("Movement", 0f);
+            }
+        }
+
+        if (Input.GetKeyDown(dashButton) && Input.GetKey(rightMove))
+        {
+            if (isJumping && canDash)
+            {
+                StartCoroutine(DashRight());
+                canDash = false;
+            }
+            else if (!isJumping)
+            {
+                StartCoroutine(DashRight());
+            }
+        }
+
+        if (Input.GetKeyDown(dashButton) && Input.GetKey(leftMove))
+        {
+            if (isJumping && canDash)
+            {
+                StartCoroutine(DashLeft());
+                canDash = false;
+            }
+            else if (!isJumping)
+            {
+                StartCoroutine(DashLeft());
+            }
+        }
+    }
+
+    IEnumerator DashRight()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().AddForce((Vector2.right * dashDistance));
+        if (isJumping)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            anim.SetFloat("JumpingAttacks", 1f);
+        }
+        else
+        {
+            anim.SetBool("Dashing", true);
+        }
+        yield return new WaitForSeconds(0.25f);
+        anim.SetBool("Dashing", false);
+        anim.SetFloat("JumpingAttacks", 0.0f);
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    IEnumerator DashLeft()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().AddForce((Vector2.left * dashDistance));
+        if (isJumping)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            anim.SetFloat("JumpingAttacks", 1f);
+        }
+        else
+        {
+            anim.SetBool("Dashing", true);
+        }
+        yield return new WaitForSeconds(0.25f);
+        anim.SetBool("Dashing", false);
+        anim.SetFloat("JumpingAttacks", 0.0f);
+        transform.eulerAngles = new Vector3(0, 180, 0);
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    void AttackInput()
+    {
+        // Your attack logic remains unchanged
+    }
+}
